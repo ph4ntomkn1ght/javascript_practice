8363636105b7d256b7e9fda665298640470c2d80
ph4ntomkn1ght <jrescam@gmail.com>
2021-11-04 14:16:08 -0600

    Agregamos el .gitignore

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /* script para un carrito de compras */
-
+//Variables
 const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+
+
+
+
